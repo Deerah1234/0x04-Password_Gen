b5dc9b645928a4d3dc57e196c289ae9fb3c2954f
Emmanuel James <chiderajames55@gmail.com>
2022-10-15 08:26:39 -0700

    Prints a random password from the ASCII table

--- main.c
+++ main.c
@@ -1,1 +1,29 @@
-Simple Password Generator in C
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+  * main - prints a random password
+  *
+  * Description - Using srand() from <stdlib.h>,
+  * time() from <time.h> library.
+  * Selects and prints ASCII character from 33-126,
+  * Alphabets: Upper and lower, Digits: 0-9 and Punctuations.
+  *
+  * Return: 0 (success)
+  */
+int main(void)
+{
+        int counter;
+
+        srand(time(NULL)); /* only do this once */
+
+        for (counter = 0; counter < 10; counter++)
+        {
+                char passwrd = (rand() % (33-126)) + 33; /* choose from ASCII from 33-126 */
+
+                printf("%c", passwrd);
+        }
+        printf("\n");
+        return (0);
+}

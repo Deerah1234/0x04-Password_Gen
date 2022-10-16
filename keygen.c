a97744f550682a3fc3fe8759e7f3af25a0bafc3e
Emmanuel James <chiderajames55@gmail.com>
2022-10-16 12:08:18 -0700

    Keygen for 101-crackme

---
+++ keygen.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Keygen for 101-crackme
+ *
+ * Description: Prints a random password that
+ * matches the decimal value from the 101-crackme.
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int cunter, rnum, idx, key;
+	char *ch = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+	char psswrd[50];
+
+	srand(time(NULL));
+	while (key != 2772)
+	{
+		cunter = idx = key = 0;
+		while (key < 2772)
+		{
+			rnum = rand() % 63;
+			psswrd[cunter] = ch[rnum];
+			key += ch[rnum];
+			cunter++;
+		}
+		while (ch[idx])
+		{
+			if (ch[idx] == 2772)
+			{
+				rnum = rand() % 63;
+				psswrd[cunter] = ch[rnum];
+				key += ch[rnum];
+				cunter++;
+				break;
+			}
+			idx++;
+		}
+	}
+	psswrd[cunter] = '\0';
+	printf("%s", psswrd);
+	return (0);
+}
+
